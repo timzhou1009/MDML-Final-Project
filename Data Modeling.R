8084331551e6a28f2b9172c00d1e155a082affbf
Ria Pinjani <ria_pinjani@hotmail.com>
2019-12-12 18:33:34 -0500

    more models

--- Data Modeling.R
+++ Data Modeling.R
@@ -1,13 +1,16 @@
 # Christina Ho, Tim Chou, Ria Pinjani #####
 ########### Data Modeling ##############
 ###########################################
+
 # Install required packages
 require(tidytext)
 require(tidyverse)
 require(tm)
-require(readr)
+library(e1071)
 
-<<<<<<< HEAD
+# set seed
+set.seed(2048)
+
 # Read data into R
 
 dat = read_csv('data/final.csv')
@@ -19,16 +22,6 @@ dat <- dat %>% rename(review.count.business = review_count.x,
                       business.star = stars.x,
                       user.star = stars.y) %>% 
   select(-c(X1, X1_1, average_stars.1))
-=======
-dat = read_csv('data/dat_clean.csv')
-
-# General guidelines for all models
-
-# Split cleaned data into 90% training and 10% testing using 10 fold cross validation
-
-# train <- 
-# test <- 
->>>>>>> a0ba2a9d90d0a3a2824b7ceaf178f0d567e873fc
 
 
 
@@ -45,35 +38,34 @@ text_dat <- dat %>% select(text, review_id, business_id)  %>% unnest_tokens(word
 data(stop_words)
 text_dat <- text_dat %>% anti_join(stop_words)
 
-# Remove neutral words 
-
-# neutral_words <- as.tibble(c('dinner', 'dish', 'dishes' ,'drink', 'pm', 'red', 
-               #    'main', 'plates', 'entrees', 'tonight', 'cup', 'le', 'fork', 'bought',
-                #   'im', 'eat', 'drinks', 'day', 'meal'))
-
-# neutral_words <- neutral_words  %>% rename(word = value)
-  
-# text_dat <- text_dat %>% anti_join(neutral_words)
-
 
 ### Pick top k frequent words, calculate frequency of each top k word
 
-# 1000 most frequent words to view
-text_dat_top_k <- text_dat %>% count(word) %>% arrange(desc(n)) %>% slice(1:1000)
+# Tried running the models for different levels of k and noted down the rmse for each
+# k <- c("50", "100", "500", "1000")
 
-# Most common words by business id 
+k <- 500
+text_dat_top_k <- text_dat %>% count(word) %>% arrange(desc(n)) %>% slice(1:k)
+
+
+# 1000 most frequent words to view
+
+
+# Most common words by business id , count for the word in each business id
 business_words <- text_dat %>% count(word, business_id, sort = T)
 
-# Top Word Occurence by Restaurant and Total
-# group business_words by business id and create column for total # of words in each business_id
+
+# group business_words by word and create column for total for word occurence in dat
 total_words <- business_words %>% 
-  group_by(business_id) %>% 
+  group_by(word) %>% 
   summarize(total = sum(n))
 
 business_words <- left_join(business_words, total_words)
 
 # filter business_words to only keep words that belong to top k 
 business_words <-  business_words %>% filter(word %in% text_dat_top_k$word) %>% arrange(business_id)
+
+
 
 # 1. Baseline; divide the number of occurence with the total number of occurence for all the top k words
 
@@ -84,127 +76,115 @@ feature_matrix1 <- business_words %>% group_by(business_id, word) %>%
 
 feature_matrix1 <- feature_matrix1 %>% cast_dtm(document = business_id, term = word, value = prop_top_k)
 
-# 2. Only include adjectives and adverbs
-
-<<<<<<< HEAD
-text_dat <- text_dat %>% 
-left_join(parts_of_speech) %>%
-  filter(pos %in% c("Adjective","Adverb")) 
-
-text_dat_top_k2 <- text_dat %>% count(word) %>% arrange(desc(n)) %>% slice(1:1000)
-=======
-# 2. Feature Engineering I; Part of Speech analysis per sentence, use these results to select top k frequent words
-
-# text in sentence
-review_sentence <- dat %>%
-  select(review_id, business_id, business.star, text) %>%
-  unnest_tokens(sentence, text, token = "sentences")
-
-# text as a whole
-review_string = unlist(dat$text) %>% 
-  paste(collapse=' ') %>% 
-  as.String
-
-# POS 
-review_pos = text_dat %>% 
-  group_by(word) %>%                                # get words
-  inner_join(parts_of_speech) %>%                   # join POS
-  count(pos) %>%                                    # count
-  mutate(prop=n/sum(n))
-
-text_adj <-review_pos[c(review_pos$pos=='Adjective'),]
-text_adv <-review_pos[c(review_pos$pos=='Adverb'),]
-
-install.packages("openNLPmodels.en", repos = "http://datacube.wu.ac.at/", type = "source")
-install.packages("opneNLP")
->>>>>>> a0ba2a9d90d0a3a2824b7ceaf178f0d567e873fc
-
-# Most common words by business id 
-business_words2 <- text_dat %>% count(word, business_id, sort = T)
-
-<<<<<<< HEAD
-# Top Word Occurence by Restaurant and Total
-# group business_words by business id and create column for total # of words in each business_id
-total_words2 <- business_words2 %>% 
-  group_by(business_id) %>% 
-  summarize(total = sum(n))
-
-business_words2 <- left_join(business_words2, total_words2)
-=======
-init_s_w = annotate(review_string, list(Maxent_Sent_Token_Annotator(),
-                                        Maxent_Word_Token_Annotator()))
->>>>>>> a0ba2a9d90d0a3a2824b7ceaf178f0d567e873fc
-
-# filter business_words to only keep words that belong to top k 
-business_words2 <-  business_words2 %>% filter(word %in% text_dat_top_k2$word) %>% arrange(business_id)
-
-# 1. Baseline; divide the number of occurence with the total number of occurence for all the top k words
-
-# Resulting training matrix should be size N (number of restaurants) x k 
-
-feature_matrix2 <- business_words2 %>% group_by(business_id, word) %>% 
-  mutate(prop_top_k = n/total) %>% select(business_id, prop_top_k) %>% distinct() 
-
-feature_matrix2 <- feature_matrix2 %>% cast_dtm(document = business_id, term = word, value = prop_top_k)
-
 
 
 # Models - 
 
-# 1 Linear Regression
+
 
 # 2 Support Vector Regression
 
-# Split cleaned data into 90% training and 10% testing using 10 fold cross validation
+# Split cleaned data into 90% training and 10% testing 
 
 smp_size <- floor(0.90 * nrow(feature_matrix1))
 
 train_ind <- sample(seq_len(nrow(feature_matrix1)), size = smp_size)
 
 train <- feature_matrix1[train_ind, ]
-labels <- dat %>% arrange(business_id) %>% select(business_id, business.star) %>% distinct() %>% select(business.star)
-train_labels <- labels[train_ind,]
-train <- train %>% as.matrix() %>% as.tibble() %>%  mutate(y = train_labels$business.star)
 
+labels <- dat %>% filter(business_id %in% business_words$business_id) %>% 
+  arrange(business_id) %>% select(business_id, business.star) %>% 
+  distinct() %>% select(business.star)
+
+train_labels <- labels[train_ind,]
+train <- train %>% as.matrix() %>% as.tibble() 
 
 test <-  feature_matrix1[-train_ind, ] %>% as.matrix() %>% as.tibble()
 test_labels <- labels[-train_ind,]
 
+# Run svm model using caret package
 
-library(caret)
-model <- train(y ~., data = train, method = 'svmLinear3')
+model <- svm(train, train_labels)
 pred <-  predict(model, test)
 pred <- round(pred*2)/2
 
+model_nb <- e1071::naiveBayes(train, as.factor(train_labels$business.star))
+pred_nb <-  predict(model_nb, test)
+
+
+# test accuracy of baseline model
+
+Metrics::rmse(test_labels$business.star, pred)
+Metrics::rmse(test_labels$business.star, as.numeric(as.vector(pred_nb)))
+
+
+# Support Vector Regression with  feature engineering 2
+
+# 2. Only include adjectives and adverbs
+
+text_dat2 <- text_dat %>% 
+  left_join(parts_of_speech) %>%
+  filter(pos %in% c("Adjective","Adverb")) 
 
 
 
-Metrics::accuracy(test_labels$business.star, pred)
+
+# 1000 most frequent words to view
+text_dat_top_k2 <- text_dat2 %>% count(word) %>% arrange(desc(n)) %>% slice(1:k)
+
+# Most common words by business id 
+business_words2 <- text_dat2 %>% count(word, business_id, sort = T)
 
 
-# 3 Support Vector Regression with  feature engineering 2
+# group business_words by business id and create column for total word occurence in dat
+total_words2 <- business_words2 %>% 
+  group_by(word) %>% 
+  summarize(total = sum(n))
+
+business_words2 <- left_join(business_words2, total_words2)
+
+# filter business_words to only keep words that belong to top k 
+business_words2 <-  business_words2 %>% filter(word %in% text_dat_top_k2$word) %>% arrange(business_id)
+
+
+feature_matrix2 <- business_words2 %>% group_by(business_id, word) %>% 
+  mutate(prop_top_k = n/total) %>% select(business_id, prop_top_k) %>% distinct() 
+
+feature_matrix2 <- feature_matrix2 %>% cast_dtm(document = business_id, term = word, value = prop_top_k)
+
+# Split cleaned data into 90% training and 10% testing 
 
 smp_size2 <- floor(0.90 * nrow(feature_matrix2))
 
 train_ind2 <- sample(seq_len(nrow(feature_matrix2)), size = smp_size2)
 
-train2 <- feature_matrix2[train_ind2, ]
-labels2 <- dat %>% arrange(business_id) %>% select(business_id, business.star) %>% distinct() %>% select(business.star)
-train_labels2 <- labels2[train_ind2,]
-train2 <- train2 %>% as.matrix() %>% as.tibble() %>%  mutate(y = train_labels2$business.star)
 
+
+train2 <- feature_matrix2[train_ind2, ]
+labels2 <- dat %>% filter(business_id %in% business_words2$business_id) %>%  arrange(business_id) %>% select(business_id, business.star) %>% distinct() %>% select(business.star)
+train_labels2 <- labels2[train_ind2,]
+train2 <- train2 %>% as.matrix() %>% as.tibble() 
 
 test2 <-  feature_matrix2[-train_ind2, ] %>% as.matrix() %>% as.tibble()
 test_labels2 <- labels2[-train_ind2,]
 
+# svm model 
 
-library(caret)
-model2 <- train(y ~., data = train2 , method = 'svmLinear3')
+
+model2 <- svm(train2, train_labels2)
 pred2 <-  predict(model2, test2)
 pred2 <- round(pred2*2)/2
 
+# naive bayes model
+
+model_nb2 <- e1071::naiveBayes(train2, as.factor(train_labels2$business.star))
+pred_nb2 <-  predict(model_nb2, test)
 
 
 
-Metrics::accuracy(test_labels2$business.star, pred2)
-# 4 Decision Tree Regression
+
+# accuracy
+
+Metrics::rmse(test_labels2$business.star, pred2)
+Metrics::rmse(test_labels2$business.star, pred_nb2)
+
