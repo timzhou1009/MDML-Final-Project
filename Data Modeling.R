2e267c7225ec4cebeed76d2d591051d2171f5b83
Chris Ho <chrisho@10-17-32-220.dynapool.wireless.nyu.edu>
2019-12-12 20:37:35 -0500

    spaces

--- Data Modeling.R
+++ Data Modeling.R
@@ -6,7 +6,10 @@
 require(tidytext)
 require(tidyverse)
 require(tm)
-library(e1071)
+require(e1071)
+require(Metrics)
+require(nnet)
+require(BBmisc)
 
 # set seed
 set.seed(2048)
@@ -122,15 +125,10 @@ pred_svm_n <- predict(model_svm_n, normalize(test))
 pred_svm_n <- round(pred_svm_n*2)/2
 
 # Run naive Bayes model
-
-
 model_nb <- e1071::naiveBayes(train, as.factor(train_labels$business.star))
 pred_nb <-  predict(model_nb, test)
 
-
-
-# test accuracy of baseline model
-
+# Test accuracy of baseline model
 Metrics::rmse(test_labels$business.star, pred_svm)
 Metrics::rmse(test_labels$business.star, pred_svm_n)
 Metrics::rmse(test_labels$business.star, as.numeric(as.vector(pred_nb)))
